1e028874ee162e485e07ffde921e1d8645a89eea
pnbao <pnbao@acworks.co.jp>
2020-03-16 12:35:06 +0900

    move load model

--- LPGAN/FUNCTION.py
+++ LPGAN/FUNCTION.py
@@ -6,6 +6,20 @@ from time import localtime, strftime
 from datetime import datetime
 from scipy import io
 
+def load_graph(frozen_graph_filename):
+    # We load the protobuf file from the disk and parse it to retrieve the 
+    # unserialized graph_def
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    # Then, we import the graph_def into a new Graph and returns it 
+    with tf.Graph().as_default() as graph:
+        # The name var will prefix every op/nodes in your graph
+        # Since we load everything in a new graph, this is not needed
+        tf.import_graph_def(graph_def, name="prefix")
+    return graph
+
 def current_time():
     return strftime("%Y-%m-%d %H:%M:%S", localtime())
 
